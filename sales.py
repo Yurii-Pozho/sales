071469cd2a492bca42e358decae4b5989aee7517
Yurii-Pozho <dusiredford92@gmail.com>
2024-07-25 21:31:12 +0300

    fifth commit

--- sales.py
+++ sales.py
@@ -5,8 +5,11 @@ import streamlit as st
 def clean(value):
     value = re.sub(r'[^\d\.,-]', '', str(value))
     value = value.replace(',', '.')
-    result = float(value)
-    return abs(int(result))
+    try:
+        result = float(value)
+        return abs(int(result))
+    except ValueError:
+        return 0 
 
 def extract_digit(description):
     match = re.search(r'(\d{4})\.', description)
@@ -19,7 +22,8 @@ upload_file = st.file_uploader("Оберіть файл Excel",type=['xlsx'])
 
 if upload_file:   
     data = pd.read_excel(upload_file)
-    data = data.rename(columns={'Unnamed: 2':'Describe','Unnamed: 5': 'Credits'}) 
+    data = data.rename(columns={'Unnamed: 2':'Describe','Unnamed: 5': 'Credits'})
+    data = data.drop([0,1]).reset_index(drop=True)    
     data['Credits'] = data['Credits'].apply(clean)
     data['Bank_acount'] = data['Describe'].apply(extract_digit)
     filtered_data = data[data['Bank_acount'].notna()]
